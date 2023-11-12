0fc121ae7011f71194b7a8255db5c4f415358d61
= <=>
2023-11-12 16:22:45 +0900

    updated

--- version-check/scripts/get_version_test.sh
+++ version-check/scripts/get_version_test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+EXIT_CODE=0
+
 echo -n "testing standard version file..............................."
 VERSION=$(./get_version.sh ../data/standard-version-file 1 "")
 if [ "$VERSION" != "v0.0.1" ]
 then
+    EXIT_CODE=1
     echo "fail"
 else
     echo "pass"
@@ -13,6 +16,7 @@ echo -n "testing pattern match version file.........................."
 VERSION=$(./get_version.sh ../data/complex-version-file 1 "version = \"(.*)\"")
 if [ "$VERSION" != "0.2.0" ]
 then
+    EXIT_CODE=1
     echo "fail"
 else
     echo "pass"
@@ -22,7 +26,10 @@ echo -n "testing pattern match version file specific occurrence......"
 VERSION=$(./get_version.sh ../data/complex-version-file 2 "version = \"(.*)\"")
 if [ "$VERSION" != "0.3.0" ]
 then
+    EXIT_CODE=1
     echo "fail"
 else
     echo "pass"
 fi
+
+exit $EXIT_CODE
