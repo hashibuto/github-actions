6b6978694ce5d5b10195fe0e267f869911af222d
= <=>
2023-11-19 14:00:29 +0900

    updated

--- version-check/scripts/get_version.sh
+++ version-check/scripts/get_version.sh
@@ -4,6 +4,12 @@ FILENAME="$1"
 OCCURRENCE="$2"
 EXPRESSION="$3"
 
+if [ ! -f "$FILENAME" ]
+then
+    echo "The version file $FILENAME does not exist"
+    exit 1
+fi
+
 if [ "$EXPRESSION" == "" ]
 then
     VERSION=$(cat $FILENAME)
